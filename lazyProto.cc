799a8559d9bd0b1bf3eb00ebdc693256527165ce
Reet Barik <reetb@Lady-MacBitch.home>
2022-10-24 13:32:44 -0700

    Unordered map to priority queue via constructor

--- lazyProto.cc
+++ lazyProto.cc
@@ -13,6 +13,54 @@
 #include <string.h>
 #include <unordered_map>
 #include <queue>
+#include <chrono>
+
+std::vector<int> max_cover(std::unordered_map<int, std::set<int>> data, int k, int theta) {
+
+	Bitmask<int> covered(theta);
+	std::vector<int> result(k, -1);
+
+
+	for (size_t i = 0; i < k; i++) {
+
+		int max = 0;
+		int max_key = -1;
+
+		for( const auto& [key, value] : data ) {
+
+			if (value.size() > max) {
+				max = value.size();
+				max_key = key;
+			}
+
+		}
+		result[i] = max_key;
+
+		for (int e: data[max_key]) {
+			covered.set(e);
+		}
+
+		for( const auto& [key, value] : data ) {
+
+			std::set<int> temp;
+			if (key != max_key) {
+				for (int e: value) {
+					if (covered.get(e)) {
+						temp.insert(e);
+					}
+				}
+				for (int e: temp) {
+    				data[key].erase(e); 
+      			}
+
+			}
+
+		}
+
+		data.erase(max_key);
+	}
+	return result;
+}
 
 
 template <typename idTy>
@@ -28,55 +76,59 @@ std::vector<int> max_cover_lazy_greedy(std::unordered_map<int, std::set<int>> da
 
 
 	CompareMaxHeap<int> cmp;
+
+	std::vector<std::pair<int, std::set<int>>> data_vec	= std::vector<std::pair<int, std::set<int>>>(data.begin(), data.end());
 	std::priority_queue<std::pair<int, std::set<int>>,
 	                	std::vector<std::pair<int, std::set<int>>>,
-	                	decltype(cmp)> pq;
+	                	decltype(cmp)> pq(data_vec.begin(), data_vec.end());
 
 	Bitmask<int> covered(theta);
-	// std::vector<int> coveredBool(theta, 0);
+	
 	std::vector<int> result(k, -1);
 	int count = 0;
-	for( const auto& [key, value] : data ) {
-		pq.push(std::make_pair(key, value));
-	}
+
 
 	while(count < k) {
 
-
+		
 		auto l = pq.top();
     	pq.pop();
     	
+    	
     	// remove RR IDs from l that are already covered. 
 		std::set<int> temp;
-    	for (auto e: l.second) {
+    	for (int e: l.second) {
       		if (covered.get(e)) {
         		temp.insert(e);
         	}
       	}
       	
+      	
       	for (int e: temp) {
     		l.second.erase(e); 
       	}
-
+      	
+      	
     	//calculate marginal gain
     	auto marginal_gain = l.second.size();
 
     	//calculate utiluty of next best
     	auto r = pq.top();
     	
-    	// if marginal of l is better than r's utility, l is the current best 
-    	if (marginal_gain > r.second.size()) {
+    	
+    	// if marginal of l is better than r's utility, l is the current best     
+    	if (marginal_gain >= r.second.size()) {
 	      	result[count] = l.first;
 	      	count += 1;
 	      	
-	      	std::set<int>::iterator itr;
 	      	for (auto e : l.second) {
 	        	covered.set(e);
 	      	}
     	}
     	// else push l's marginal into the heap 
 	    else {
-	      pq.push(l);
+	    	pq.push(l);
+	    	
 	    }
 
 	}
@@ -91,7 +143,7 @@ int main(int argc, char *argv[]) {
 	std::unordered_map<int, std::set<int>> data;
 	std::ifstream file;
 	file.open("coverage.txt");
-	int count = 0; int theta = -1;
+	int count = 0; int theta = -1, k = 300;
 
 	std::string line;
 	while(getline(file, line))
@@ -107,9 +159,25 @@ int main(int argc, char *argv[]) {
 	    data[++count] = temp;
 	}
 	
-	auto S = max_cover_lazy_greedy(data, 10, theta);
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	auto S1 = max_cover_lazy_greedy(data, k, theta);
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-	for (auto e: S)
+	std::cout << "Time taken for lazy greedy = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+	for (auto e: S1)
+		std::cout << e << '\t';
+
+	std::cout << '\n';
+
+
+	begin = std::chrono::steady_clock::now();
+	auto S2 = max_cover(data, k, theta);
+	end = std::chrono::steady_clock::now();
+
+	std::cout << "Time taken for naive greedy = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+	for (auto e: S2)
 		std::cout << e << '\t';
 
 	return 0;
